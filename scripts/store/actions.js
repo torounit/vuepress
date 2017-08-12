d904b020e82ee16de3cb94eabcb1f235593fe5a3
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2017-08-12 14:46:24 +0900

    saparetae period class

--- scripts/store/actions.js
+++ scripts/store/actions.js
@@ -1,62 +1,8 @@
 import * as types from './mutation-types';
 import wp from '../utility/api'
+import Period from '../utility/Period';
 
 wp.vpPost = wp.registerRoute('vuepress/v1', '/post/(?P<url>)');
-
-class Period {
-
-  constructor() {
-    this._year = null;
-    this._month = null;
-    this._day = null
-  }
-
-
-  set year(value) {
-    this._year = value;
-  }
-
-  set month(value) {
-    this._month = value;
-  }
-
-  set day(value) {
-    this._day = value;
-  }
-
-  firstDay() {
-    if (this._year === null) {
-      return null;
-    }
-
-    if(this._month === null) {
-      return new Date(this._year, 0, 1, 0, 0);
-    }
-
-    if(this._day === null) {
-      return new Date(this._year, this._month - 1, 1, 0, 0);
-    }
-
-    return new Date(this._year, this._month - 1, this._day, 0, 0);
-
-  }
-  lastDay(){
-    if (this._year === null) {
-      return null;
-    }
-
-    if(this._month === null) {
-      return new Date(this._year + 1, 0, 0, 0, 0);
-    }
-
-    if(this._day === null) {
-      return new Date(this._year, this._month, 0, 0, 0);
-    }
-
-    return new Date(this._year, this._month - 1, this._day + 1, 0, 0);
-
-  }
-}
 
 export const fetchPosts = async ({commit}, route) => {
 
---
+++ scripts/utility/Period.js
@@ -0,0 +1,54 @@
+export default class Period {
+
+  constructor() {
+    this._year = null;
+    this._month = null;
+    this._day = null
+  }
+
+
+  set year(value) {
+    this._year = value;
+  }
+
+  set month(value) {
+    this._month = value;
+  }
+
+  set day(value) {
+    this._day = value;
+  }
+
+  firstDay() {
+    if (this._year === null) {
+      return null;
+    }
+
+    if(this._month === null) {
+      return new Date(this._year, 0, 1, 0, 0);
+    }
+
+    if(this._day === null) {
+      return new Date(this._year, this._month - 1, 1, 0, 0);
+    }
+
+    return new Date(this._year, this._month - 1, this._day, 0, 0);
+
+  }
+  lastDay(){
+    if (this._year === null) {
+      return null;
+    }
+
+    if(this._month === null) {
+      return new Date(this._year + 1, 0, 0, 0, 0);
+    }
+
+    if(this._day === null) {
+      return new Date(this._year, this._month, 0, 0, 0);
+    }
+
+    return new Date(this._year, this._month - 1, this._day + 1, 0, 0);
+
+  }
+}
